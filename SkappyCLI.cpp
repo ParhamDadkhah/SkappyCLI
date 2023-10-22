81e10d6e1b55080d577174a746f620c2f6fd3917
ParhamDadkhah <parham.dadkhah84@gmail.com>
2023-10-22 15:30:20 +0330

    Early program design

--- SkappyCLI.cpp
+++ SkappyCLI.cpp
@@ -1,20 +1,37 @@
-// SkappyCLI.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cryptopp/sha.h>
 
+std::string GenerateKey(std::string PassPhrase);
+void BeginEncrypt();
+void BeginDecrypt();
+
+void PrintWelcome()
+{
+	system("cls");
+	using namespace std;
+	cout << "Hello and welcome to Skappy. Skappy will encrypt whatever file you desire with AES-CFB-256 to make sure your file is secure." << endl;
+	cout << "Please select an option:" << endl << "[1] Encrypt\n[2] Decrypt\n[3] Exit\n";
+}
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int Option;
+	do
+	{
+		PrintWelcome();
+		std::cin >> Option;
+	} while (Option <= 3 && Option >= 0);
+
+	switch (Option)
+	{
+		case 1:
+			BeginEncrypt();
+			break;
+		case 2:
+			BeginDecrypt();
+			break;
+		case 3:
+			return 0;
+			break;
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
